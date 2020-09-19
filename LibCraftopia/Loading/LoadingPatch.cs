b3ad736da6992ed0a4710c998d8d32d85ce221fe
nanofi <nanogenomu@gmail.com>
2020-09-19 23:22:56 +0900

    Inject loading coroutine

---
+++ LibCraftopia/Loading/LoadingPatch.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using Oc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibCraftopia.Loading
+{
+    public class LoadingPatch
+    {
+        [HarmonyPatch(typeof(OcGameSceneTransformManager), "IELoadScenesAsync")]
+        [HarmonyPostfix]
+        static IEnumerator PostfixIELoadScenesAsync(IEnumerator original, bool needsStabilization)
+        {
+            Logger.Inst.LogInfo("Mods' loading procedures start");
+            var loaders = LoadingManager.Inst.OnLoadScene(needsStabilization);
+            while (loaders.MoveNext())
+            {
+                yield return loaders.Current;
+            }
+            Logger.Inst.LogInfo("Mods' loading procedures end");
+            while (original.MoveNext())
+            {
+                yield return original.Current;
+            }
+        }
+
+        [HarmonyPatch(typeof(OcGameMng), "UnloadGameScene")]
+        [HarmonyPostfix]
+        static IEnumerator PostfixUnloadGameScene(IEnumerator original)
+        {
+
+            while (original.MoveNext())
+            {
+                yield return original.Current;
+            }
+            LoadingManager.Inst.OnGameSceneUnloaded();
+        }
+    }
+}
