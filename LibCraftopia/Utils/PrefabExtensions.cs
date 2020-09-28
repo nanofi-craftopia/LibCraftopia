02887ba89eda4d94c5baa5d6c8d55647cd6d14ea
nanofi <nanogenomu@gmail.com>
2020-09-29 03:57:54 +0900

    Utility for cloning prefab

---
+++ LibCraftopia/Utils/PrefabExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LibCraftopia.Utils
+{
+    public static class PrefabExtensions
+    {
+        private static GameObject parent;
+
+        private static GameObject Parent
+        {
+            get
+            {
+                if (!parent)
+                {
+                    parent = new GameObject("LibCraftopia.PrefabParent");
+                    UnityEngine.Object.DontDestroyOnLoad(parent);
+                    parent.SetActive(false);
+                }
+                return parent;
+            }
+        }
+
+        public static T InstantiateClone<T>(this T original, string name) where T : UnityEngine.Object
+        {
+            var prefab = UnityEngine.Object.Instantiate(original, Parent.transform);
+            prefab.name = name;
+            return prefab;
+        }
+    }
+}
